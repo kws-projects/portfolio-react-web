41e116c1cd8fc15af63307659f4a5b11795d250f
Kenneth Wong <kwwong2210@gmail.com>
2024-05-08 00:58:20 +0800

    Update navbar style

--- src/components/Header/Navbar.tsx
+++ src/components/Header/Navbar.tsx
@@ -6,19 +6,19 @@ const Navbar = () => {
     const { t } = useTranslation();
 
     return (
-        <nav className="flex items-center justify-between w-full max-w-screen-lg mx-28 border-b border-gray-200 text-sm">
-            <Link to="/" className="text-lg" >{t('website_name')}</Link>
+        <nav className="flex items-center justify-between w-full h-14 max-w-screen-lg mx-0 border-b border-gray-200 text-sm md:mx-14 lg:mx-28">
+            <Link to="/" className="text-lg ml-4 pt-1" >{t('website_name')}</Link>
 
             {/* navbar toggle */}
-            <div className="hidden">=</div>
+            <div className="flex md:hidden">=</div>
 
-            <div>
+            <div className="hidden md:flex">
                 <ul>
                     {navItems.map(item => (
                         <li key={item.id} className="group/nav-2-lv relative float-left" >
                             {item.link 
-                                ? <Link to={item.link} className="block pt-4 px-5 pb-3.5 border-b border-transparent hover:border-b-gray-800" >{item.title}</Link> 
-                                : <div className="block pt-4 px-5 pb-3.5 cursor-pointer border-b border-transparent hover:border-b-gray-800" >{item.title}</div>
+                                ? <Link to={item.link} className="block pt-5 px-5 pb-3.5 border-b border-transparent hover:border-b-gray-800" >{item.title}</Link> 
+                                : <div className="block pt-5 px-5 pb-3.5 cursor-pointer border-b border-transparent hover:border-b-gray-800" >{item.title}</div>
                             }
 
                             {item.subItems && <ul className="hidden absolute left-0 group-hover/nav-2-lv:block w-44 bg-white rounded-b-lg border border-gray-200">
