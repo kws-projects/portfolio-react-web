213031817f200c2baa63224d2dd256f8c8b6f5fb
Kenneth Wong <kwwong2210@gmail.com>
2024-05-08 03:02:10 +0800

    Update navbar responsive and data file

--- src/components/Header/Navbar.tsx
+++ src/components/Header/Navbar.tsx
@@ -1,40 +1,58 @@
+import { useState } from 'react'
 import { Link } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
-import { navItems } from '../../data';
+import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
+import { navItems } from '../../data/navItems';
+
+const useNavbarToggle = () => {
+    const [ isNavbarExpended, setIsNavbarExpended ] = useState(false)
+
+    const handleNavbarToggle = () => {
+        setIsNavbarExpended(prev => !prev)
+    }
+
+    return { isNavbarExpended, handleNavbarToggle }
+}
 
 const Navbar = () => {
     const { t } = useTranslation();
+
+    const { isNavbarExpended, handleNavbarToggle } = useNavbarToggle()
 
     return (
         <nav className="flex items-center justify-between w-full h-14 max-w-screen-lg mx-0 border-b border-gray-200 text-sm md:mx-14 lg:mx-28">
             <Link to="/" className="text-lg ml-4 pt-1" >{t('website_name')}</Link>
 
-            {/* navbar toggle */}
-            <div className="flex md:hidden">=</div>
+            <div className="flex pt-1 md:hidden" onClick={handleNavbarToggle}>
+                {isNavbarExpended
+                    ? <div className="py-4 px-6 cursor-pointer"><AiOutlineClose /></div>
+                    : <div className="py-4 px-6 cursor-pointer"><AiOutlineMenu /></div>
+                }
+            </div>
 
-            <div className="hidden md:flex">
-                <ul>
+            <div className={`${isNavbarExpended ? 'flex' : 'hidden'} absolute top-14 w-full md:relative md:top-0 md:flex md:w-fit`}>
+                <ul className="w-full md:w-fit">
                     {navItems.map(item => (
-                        <li key={item.id} className="group/nav-2-lv relative float-left" >
+                        <li key={item.id} className="group/nav-2-lv relative float-left w-full md:w-fit" >
                             {item.link 
-                                ? <Link to={item.link} className="block pt-5 px-5 pb-3.5 border-b border-transparent hover:border-b-gray-800" >{item.title}</Link> 
-                                : <div className="block pt-5 px-5 pb-3.5 cursor-pointer border-b border-transparent hover:border-b-gray-800" >{item.title}</div>
+                                ? <Link to={item.link} className="block pt-5 px-5 pb-3.5 w-full border-b border-transparent hover:bg-gray-100 md:hover:border-b-gray-800 md:hover:bg-white" >{item.title}</Link> 
+                                : <div className="block pt-5 px-5 pb-3.5 w-full cursor-pointer border-b border-transparent hover:bg-gray-100 md:hover:border-b-gray-800 md:hover:bg-white" >{item.title}</div>
                             }
 
-                            {item.subItems && <ul className="hidden absolute left-0 group-hover/nav-2-lv:block w-44 bg-white rounded-b-lg border border-gray-200">
+                            {item.subItems && <ul className="hidden relative left-0 group-hover/nav-2-lv:block w-full bg-white border border-gray-200 md:absolute md:w-44 md:rounded-b-lg">
                                 {item.subItems.map((item, i, arr) => (
                                     <li key={item.id} className="group/nav-3-lv relative" >
                                         {item.link 
-                                            ? <Link to={item.link} className={`block pt-4 px-5 pb-3.5 hover:bg-gray-100 ${i===arr.length-1&&'rounded-b-lg'}`} >{item.title}</Link> 
-                                            : <div className={`block pt-4 px-5 pb-3.5 cursor-pointer hover:bg-gray-100 ${i===arr.length-1&&'rounded-b-lg'}`} >{item.title}</div>
+                                            ? <Link to={item.link} className={`block pt-4 pr-5 pl-10 pb-3.5 hover:bg-gray-100 ${i===arr.length-1&&'md:rounded-b-lg'} md:pl-5`} >{item.title}</Link>
+                                            : <div className={`block pt-4 pr-5 pl-10 pb-3.5 cursor-pointer hover:bg-gray-100 ${i===arr.length-1&&'md:rounded-b-lg'} md:pl-5`} >{item.title}</div>
                                         }
 
-                                        {item.subItems && <ul className="hidden absolute top-0 left-44 group-hover/nav-3-lv:block w-44 bg-white rounded-b-lg rounded-tr-lg border border-gray-200">
+                                        {item.subItems && <ul className="hidden relative top-0 left-0 group-hover/nav-3-lv:block w-full bg-white border border-gray-200 md:absolute md:left-44 md:w-44 md:rounded-b-lg md:rounded-tr-lg">
                                             {item.subItems.map((item, i, arr) => (
-                                                <li key={item.id} className='relative' >
+                                                <li key={item.id} className='relative w-full' >
                                                     {item.link 
-                                                        ? <Link to={item.link} className={`block pt-4 px-5 pb-3.5 hover:bg-gray-100 ${i===0&&'rounded-tr-lg'} ${i===arr.length-1&&'rounded-b-lg'}`} >{item.title}</Link> 
-                                                        : <div className={`block pt-4 px-5 pb-3.5 cursor-pointer hover:bg-gray-100 ${i===0&&'rounded-tr-lg'} ${i===arr.length-1&&'rounded-b-lg'}`} >{item.title}</div>
+                                                        ? <Link to={item.link} className={`block pt-4 pr-5 pl-16 pb-3.5 hover:bg-gray-100 ${i===0&&'md:rounded-tr-lg'} ${i===arr.length-1&&'md:rounded-b-lg'} md:pl-5`} >{item.title}</Link> 
+                                                        : <div className={`block pt-4 pr-5 pl-16 pb-3.5 cursor-pointer hover:bg-gray-100 ${i===0&&'md:rounded-tr-lg'} ${i===arr.length-1&&'md:rounded-b-lg'} md:pl-5`} >{item.title}</div>
                                                     }
                                                 </li>
                                             ))}
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,9 +2,12 @@ import Navbar from './Navbar'
 
 const Header = () => {
     return (
-        <header className="fixed top-0 left-0 flex justify-center w-full">
-            <Navbar />
-        </header>
+        <>
+            <header className="fixed top-0 left-0 flex justify-center w-full bg-gray-50">
+                <Navbar />
+            </header>
+            <div className="mt-14"></div>
+        </>
     )
 }
 
