47df9068282d9cfa6e3a8f59bd6e371e0c9b375e
Kenneth Wong <kwwong2210@gmail.com>
2024-05-12 01:31:09 +0800

    Add about and blog title banner

--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -1,12 +1,17 @@
 import { useTranslation } from 'react-i18next'
 import usePageTitle from '../../hooks/usePageTitle'
+import useScrollToTop from '../../hooks/useScrollToTop'
+import PageTitleSketch from '../../components/sketches/PageTitleSketch'
 
 const About = () => {
     const { t } = useTranslation()
+    useScrollToTop()
     usePageTitle(t('about_document_title'))
 
     return (
-        <main>About</main>
+        <main>
+            <PageTitleSketch title={t('about_title')} />
+        </main>
     )
 }
 
--- src/pages/Blog/index.tsx
+++ src/pages/Blog/index.tsx
@@ -1,12 +1,17 @@
 import { useTranslation } from 'react-i18next'
 import usePageTitle from '../../hooks/usePageTitle'
+import useScrollToTop from '../../hooks/useScrollToTop'
+import PageTitleSketch from '../../components/sketches/PageTitleSketch'
 
 const Blog = () => {
     const { t } = useTranslation()
+    useScrollToTop()
     usePageTitle(t('blog_document_title'))
 
     return (
-        <main>Blog</main>
+        <main>
+            <PageTitleSketch title={t('blog_title')} />
+        </main>
     )
 }
 
