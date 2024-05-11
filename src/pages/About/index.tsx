1f4ec5fd5d2d37568e3bc97a3ab91a1b6e56333b
Kenneth Wong <kwwong2210@gmail.com>
2024-05-12 02:12:16 +0800

    Update home self intro

--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -3,6 +3,7 @@ import usePageTitle from '../../hooks/usePageTitle'
 import useScrollToTop from '../../hooks/useScrollToTop'
 import PageTitleSketch from '../../components/sketches/PageTitleSketch'
 import Section from '../../components/ui/Section'
+import ContactPortal from '../../components/ContactPortal'
 
 const About = () => {
     const { t } = useTranslation()
@@ -13,26 +14,34 @@ const About = () => {
         <main>
             <PageTitleSketch title={t('about_title')} />
 
+            <Section showBreakline={false} >
+                <div className="flex flex-col md:flex-row justify-between items-center space-x-0 md:space-x-20 space-y-12 md:space-y-0 px-8 md:px-12">
+                    <img className="w-44 h-44 md:w-52 md:h-52 lg:w-64 lg:h-64 rounded-lg"
+                    src="/assets/images/profile-image.jpeg" alt="Profile" />
+
+                    <div className="flex flex-col justify-start items-center md:items-start space-y-6 px-6 md:px-0">
+                        <span className="text-2xl">{t('about_self_intro_heading')}</span>
+                        <p className="text-center md:text-left">{t('about_self_intro_content')}</p>
+                        <ContactPortal />
+                    </div>
+                </div>
+                
+            </Section>
+
             <Section 
                 title={t('about_skills_title')} 
                 description={t('about_skills_description')} 
-                className=""
-            ></Section>
+            >
+            </Section>
 
-            <Section 
-                title={t('about_cv_work_experience_title')} 
-                className=""
-            ></Section>
+            <Section title={t('about_cv_work_experience_title')} >
+            </Section>
 
-            <Section 
-                title={t('about_cv_certifications_title')} 
-                className=""
-            ></Section>
+            <Section title={t('about_cv_certifications_title')} >
+            </Section>
 
-            <Section 
-                title={t('about_cv_education_title')} 
-                className=""
-            ></Section>
+            <Section title={t('about_cv_education_title')} >   
+            </Section>
 
         </main>
     )
