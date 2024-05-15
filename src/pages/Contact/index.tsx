e68214cb613c99a333ca8abe9b00da72421acd6b
Kenneth Wong <kwwong2210@gmail.com>
2024-05-15 22:08:12 +0800

    Update about banner and contact style

--- src/pages/Contact/index.tsx
+++ src/pages/Contact/index.tsx
@@ -19,7 +19,7 @@ const Contact = () => {
                 style={{ marginTop: '-2rem', height: '-webkit-fill-available' }}
                 showBreakline={false}
             >
-                <div className="flex flex-col justify-between items-center w-full h-full mt-20 sm:mt-0 py-16 max-w-screen-md max-h-96 rounded-none sm:rounded-lg bg-white/80 shadow-custom">
+                <div className="flex flex-col justify-between items-center w-full h-full mt-20 sm:mt-0 py-16 min-h-80 max-w-screen-md max-h-96 rounded-none sm:rounded-lg bg-white/80 shadow-custom">
                     <span className="text-3xl">{t('contact_title')}</span>
 
                     <div className="flex flex-col justify-center items-center space-y-2">
