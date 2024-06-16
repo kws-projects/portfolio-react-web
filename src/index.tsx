0064ed03a10010587b5fd87e2a39d601ceafd18f
Kenneth Wong <kwwong2210@gmail.com>
2024-06-16 15:12:01 +0800

    Format source code

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './index.css'
 import App from './App'
 import ReactGA from 'react-ga4'
-import * as Sentry from "@sentry/react";
+import * as Sentry from '@sentry/react'
 
 ReactGA.initialize(process.env.REACT_APP_GA_MEASUREMENT_ID!)
 
@@ -17,11 +17,11 @@ Sentry.init({
   // Performance Monitoring
   tracesSampleRate: 1.0, //  Capture 100% of the transactions
   // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
-  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+  tracePropagationTargets: ['localhost', /^https:\/\/yourserver\.io\/api/],
   // Session Replay
   replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
   replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
-});
+})
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 
