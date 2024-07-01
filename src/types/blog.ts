3de30a7be13ec9880e7d236e19f7159c6e3852ab
Kenneth Wong <kwwong2210@gmail.com>
2024-07-01 09:16:06 +0800

    feat(PP-48): create blog details page

--- src/types/blog.ts
+++ src/types/blog.ts
@@ -1,15 +1,35 @@
-enum BlogStatus {
+export enum BlogStatus {
     Draft = 'DRAFT',
     Published = 'PUBLISHED',
 }
 
-export type BlogType = {
+export type Blog = {
     id: number,
     status: BlogStatus,
     author: string,
     category: string,
     titleEn: string,
     descriptionEn: string,
+    createdAt: Date,
+    createdBy: string,
+    updatedAt: Date,
+    updatedBy: string,
+}
+
+export enum BlogNodeType {
+    MD = 'MD',
+    HTML = 'HTML',
+    Image = 'IMAGE',
+    Video = 'VIDEO',
+    P5Sketch = 'P5_SKETCH',
+}
+
+export type BlogNode = {
+    id: number,
+    blogId: number,
+    order: number,
+    type: BlogNodeType,
+    fileName: string,
     createdAt: Date,
     createdBy: string,
     updatedAt: Date,
