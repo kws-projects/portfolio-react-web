0642cbd56590e14464aa620d112a625540faacc9
Kenneth Wong <kwwong2210@gmail.com>
2024-08-10 16:37:54 +0800

    refactor(PP-88): refactor component with new configs and constants

--- src/components/Footer/Version/index.tsx
+++ src/components/Footer/Version/index.tsx
@@ -1,28 +1,21 @@
 import { IoIosGitBranch } from 'react-icons/io'
+import { envConfig } from '../../../config'
+import { getReleaseType } from '../../../constant/appEnv'
 
 const Version = () => {
-  const getReleaseLabel = () => {
-    switch (process.env.REACT_APP_ENV) {
-      case 'PROD':
-        return 'Release'
-      default:
-        return 'Develop'
-    }
-  }
-
   return (
     <a
       className="group/version flex items-center gap-x-1 text-xs px-3 py-1 bg-gray-200 group-hover/version:bg-gray-400 rounded-md cursor-pointer"
-      href="https://github.com/kws-projects/portfolio-react-web/releases"
+      href={envConfig.GITHUB_RELEASE_URL}
       target="_blank"
       rel="noreferrer"
     >
       <IoIosGitBranch className="fill-gray-500 group-hover/version:fill-gray-800" />
       <span className="text-gray-500 group-hover/version:text-gray-800">
-        {getReleaseLabel()}
+        {getReleaseType()}
       </span>
       <span className="text-gray-500 group-hover/version:text-gray-800">
-        {`v${process.env.REACT_APP_BUILD_VERSION_NO}`}
+        {`v${envConfig.VERSION_NO}`}
       </span>
     </a>
   )
