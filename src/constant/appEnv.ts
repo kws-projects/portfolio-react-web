3f593b0cf6149675ec893712edf0935ff08a0db6
Kenneth Wong <kwwong2210@gmail.com>
2024-08-10 16:30:56 +0800

    refactor(PP-88): refactor configs and constants

---
+++ src/constant/appEnv.ts
@@ -0,0 +1,12 @@
+import { envConfig } from '../config'
+
+export enum AppEnv {
+  DEV = 'DEV',
+  PROD = 'PROD',
+}
+
+export const getReleaseType = () =>
+  ({
+    [AppEnv.DEV]: 'Develop',
+    [AppEnv.PROD]: 'Release',
+  })[envConfig.NODE_ENV]
